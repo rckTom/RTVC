d40deaea40d4745392637c2c5b548ca66b02432b
raketenolli <oarend@gmail.com>
2015-11-10 12:59:52 +0100

    Testcode zum Servo verfahren
    Verfährt jede Sekunde um "90°" in eine Richtung. Die Servo-Library geht
    davon aus, dass ein Servo immer 180°-Bewegungsfreiheit hat. Die
    D05010MG-Servos haben nur 90°

---
+++ Software/servo_sweep_90steps/servo_sweep_90steps.ino
@@ -0,0 +1,25 @@
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(9, 900, 2100);  // attaches the servo on pin 9 to the servo object
+								 // 900 µs is 0°, 2100 µs is 90° for D05010MG servos
+} 
+ 
+ 
+void loop() 
+{ 
+  myservo.write(90);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+  myservo.write(90);
+  delay(1000);
+  myservo.write(0);
+  delay(1000);
+} 
