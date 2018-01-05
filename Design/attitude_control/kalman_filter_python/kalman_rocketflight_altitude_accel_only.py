3e7913d2ee971242d68b3f864422167d6ed53e08
raketenolli <oarend@gmail.com>
2018-01-05 13:13:10 +0100

    Kalman filter with only accelerometer data

---
+++ Design/attitude_control/kalman_filter_python/kalman_rocketflight_altitude_accel_only.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# state transformation matrix
+A = np.matrix([[1, 0.01, 0],[0, 1, 0.01], [0, 0, 1]])
+
+# initial state (known or estimated)
+X = np.matrix([[0.0],[0.0],[0.0]])
+
+# initial error covariance
+P = np.matrix(np.identity(3))
+
+# system error covariance
+Q = 0.01 * np.matrix(np.identity(3))
+
+# measurement to state translation
+H = np.matrix([[0,0,1.0]])
+
+# measurement noise covariance
+R = np.matrix([[1.0]])
+
+X_prio_hist = []
+P_prio_hist = []
+K_hist = []
+X_hist = []
+P_hist = []
+
+Z=np.loadtxt("z_alti_accel.csv")
+Z=Z[:,1]
+v=np.loadtxt("v.csv")
+a=np.loadtxt("a.csv")
+
+# # updating K and P
+# for z in Z:
+#     # prediction
+#     X_prio = A*X
+#     P_prio = A*P*A.T + Q
+    
+#     # correction
+#     K = P_prio*H.T*np.linalg.inv(H*P_prio*H.T+R)
+#     X = X_prio + K*(np.matrix(z).T-H*X_prio)
+#     P = (np.matrix(np.identity(3)) - K*H)*P_prio
+    
+#     # save history
+#     X_prio_hist.append(X_prio)
+#     P_prio_hist.append(P_prio)
+#     K_hist.append(K)
+#     X_hist.append(X)
+#     P_hist.append(P)
+
+# converged K and P from last run
+K = np.matrix([[0.00086076],
+               [0.00904875],
+               [0.09512492]])
+
+P = np.matrix([[  1.72873653e+02,   3.17903414e+01,   8.60761712e-04],
+               [  3.17903414e+01,   8.08000951e+00,   9.04875078e-03],
+               [  8.60761712e-04,   9.04875078e-03,   9.51249220e-02]])
+
+for z in Z:
+    # prediction
+    X_prio = A*X
+    
+    # correction
+    X = X_prio + K*(np.matrix(z).T-H*X_prio)
+    
+    # save history
+    X_prio_hist.append(X_prio)
+    X_hist.append(X)
+
+print("Final Kalman gain matrix K")
+print(K)
+print("Final error covariance matrix P")
+print(P)
+
+h_hist = np.array(X_hist)[:,0,0]
+v_hist = np.array(X_hist)[:,1,0]
+a_hist = np.array(X_hist)[:,2,0]
+
+t_apo = np.argmax((h_hist > 10)*(v_hist<0))
+h_apo = h_hist[t_apo]
+
+# plt.plot(Z[:,0])
+plt.plot(h_hist)
+plt.plot(v)
+plt.plot(v_hist)
+# plt.plot(Z[:,1])
+plt.plot(Z)
+plt.plot(a_hist)
+plt.scatter(t_apo, h_apo, c="red")
+plt.show()
